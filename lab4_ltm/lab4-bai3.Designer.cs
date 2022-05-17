b213b1dca10fcfb920cd27ed435b2b1649c9e8be
20520796 <104158070+20520796@users.noreply.github.com>
2022-05-17 22:17:43 +0700

    Update lab4-bai3.Designer.cs

--- lab4_ltm/lab4-bai3.Designer.cs
+++ lab4_ltm/lab4-bai3.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace lab4_ltm
 {
-    partial class lab4_bai3
+    partial class Bai03
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -19,91 +19,145 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.btnSave = new System.Windows.Forms.Button();
-            this.tbContent = new System.Windows.Forms.TextBox();
-            this.tbFilePath = new System.Windows.Forms.TextBox();
+            this.rtbContent = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tbURL = new System.Windows.Forms.TextBox();
-            this.btnDownload = new System.Windows.Forms.Button();
+            this.btDOWLOAD = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btOPEN = new System.Windows.Forms.Button();
+            this.btDELETE = new System.Windows.Forms.Button();
+            this.btCLOSE = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnSave
+            // rtbContent
             // 
-            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(653, 57);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(81, 42);
-            this.btnSave.TabIndex = 4;
-            this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
+            this.rtbContent.Location = new System.Drawing.Point(64, 104);
+            this.rtbContent.Name = "rtbContent";
+            this.rtbContent.Size = new System.Drawing.Size(577, 301);
+            this.rtbContent.TabIndex = 8;
+            this.rtbContent.Text = "";
+            this.rtbContent.TextChanged += new System.EventHandler(this.rtbContent_TextChanged);
             // 
-            // tbContent
+            // label1
             // 
-            this.tbContent.Font = new System.Drawing.Font("Verdana", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbContent.Location = new System.Drawing.Point(31, 125);
-            this.tbContent.Multiline = true;
-            this.tbContent.Name = "tbContent";
-            this.tbContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbContent.Size = new System.Drawing.Size(738, 537);
-            this.tbContent.TabIndex = 5;
-            // 
-            // tbFilePath
-            // 
-            this.tbFilePath.Font = new System.Drawing.Font("Verdana", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbFilePath.Location = new System.Drawing.Point(32, 62);
-            this.tbFilePath.Name = "tbFilePath";
-            this.tbFilePath.Size = new System.Drawing.Size(576, 34);
-            this.tbFilePath.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "URL:";
             // 
             // tbURL
             // 
-            this.tbURL.Font = new System.Drawing.Font("Verdana", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbURL.Location = new System.Drawing.Point(32, 10);
-            this.tbURL.Name = "tbURL";
-            this.tbURL.Size = new System.Drawing.Size(576, 34);
-            this.tbURL.TabIndex = 1;
+            this.tbURL.Location = new System.Drawing.Point(64, 20);
+            this.tbURL.Name = "txtURL";
+            this.tbURL.Size = new System.Drawing.Size(577, 22);
+            this.tbURL.TabIndex = 10;
             // 
-            // btnDownload
+            // btDownload
             // 
-            this.btnDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDownload.Location = new System.Drawing.Point(624, 5);
-            this.btnDownload.Name = "btnDownload";
-            this.btnDownload.Size = new System.Drawing.Size(145, 42);
-            this.btnDownload.TabIndex = 3;
-            this.btnDownload.Text = "Download";
-            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btDOWLOAD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDOWLOAD.Location = new System.Drawing.Point(647, 13);
+            this.btDOWLOAD.Name = "btDownload";
+            this.btDOWLOAD.Size = new System.Drawing.Size(140, 35);
+            this.btDOWLOAD.TabIndex = 11;
+            this.btDOWLOAD.Text = "DOWNLOAD";
+            this.btDOWLOAD.UseVisualStyleBackColor = true;
+            this.btDOWLOAD.Click += new System.EventHandler(this.btDOWLOAD_Click);
             // 
-            // lab4_bai3
+            // txtPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(64, 62);
+            this.tbPath.Name = "txtPath";
+            this.tbPath.Size = new System.Drawing.Size(577, 22);
+            this.tbPath.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Path:";
+            // 
+            // btOpen
+            // 
+            this.btOPEN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btOPEN.Location = new System.Drawing.Point(647, 62);
+            this.btOPEN.Name = "btOpen";
+            this.btOPEN.Size = new System.Drawing.Size(140, 35);
+            this.btOPEN.TabIndex = 15;
+            this.btOPEN.Text = "OPEN";
+            this.btOPEN.UseVisualStyleBackColor = true;
+            this.btOPEN.Click += new System.EventHandler(this.btOPEN_Click);
+            // 
+            // btDelete
+            // 
+            this.btDELETE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDELETE.Location = new System.Drawing.Point(647, 114);
+            this.btDELETE.Name = "btDelete";
+            this.btDELETE.Size = new System.Drawing.Size(140, 35);
+            this.btDELETE.TabIndex = 16;
+            this.btDELETE.Text = "DELETE";
+            this.btDELETE.UseVisualStyleBackColor = true;
+            this.btDELETE.Click += new System.EventHandler(this.btDELETE_Click);
+            // 
+            // btClose
+            // 
+            this.btCLOSE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCLOSE.Location = new System.Drawing.Point(647, 166);
+            this.btCLOSE.Name = "btClose";
+            this.btCLOSE.Size = new System.Drawing.Size(140, 35);
+            this.btCLOSE.TabIndex = 17;
+            this.btCLOSE.Text = "CLOSE";
+            this.btCLOSE.UseVisualStyleBackColor = true;
+            this.btCLOSE.Click += new System.EventHandler(this.btCLOSE_Click);
+            // 
+            // Bai3_Lab4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 673);
-            this.Controls.Add(this.btnDownload);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.tbContent);
-            this.Controls.Add(this.tbFilePath);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btCLOSE);
+            this.Controls.Add(this.btDELETE);
+            this.Controls.Add(this.btOPEN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btDOWLOAD);
             this.Controls.Add(this.tbURL);
-            this.Name = "lab4_bai3";
-            this.Text = "Download HTML từ web";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbContent);
+            this.Name = "Bai3_Lab4";
+            this.Text = "Bai3_Lab4";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+
         #endregion
 
-        private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.TextBox tbContent;
-        private System.Windows.Forms.TextBox tbFilePath;
-        private System.Windows.Forms.TextBox tbURL;
-        private System.Windows.Forms.Button btnDownload;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private RichTextBox rtbContent;
+        private TextBox tbPath;
+        private TextBox tbURL;
+        private Button btDOWLOAD;
+        private Button btOPEN;
+        private Button btDELETE;
+        private Button btCLOSE;
     }
 }
